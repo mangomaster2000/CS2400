4cd4561c92fbfc7cb3998803b99c2bc31d7c83f1
Joshua Ng <jtng@cpp.edu>
2024-09-09 23:10:32 -0700

    edited readme and added new link

--- Projects/project0.java
+++ Projects/project0.java
@@ -3,5 +3,6 @@ public class project0 {
         // Print a custom message
         //in the new java you dont need public static void, and you dont need the parameters string args! wowwwww
         System.out.println("Hello, Class! This is Josh.");
+        System.out.println("Goodbye!");
     }
 }
--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 ## https://github.com/mangomaster2000/CS2400/tree/Josh
 
 
-## https://youtu.be/gAdkQ2AEFNU
+## https://youtu.be/4AtV8IHwFI8
 
 ## Screencast Software: OBS Studio 30.2.3 (64 bit)
 
