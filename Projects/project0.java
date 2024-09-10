71b55d7cc66048cdb3e92c57aa19cf6679ac3cd1
Joshua Ng <jtng@cpp.edu>
2024-09-09 22:20:04 -0700

    first

---
+++ Projects/project0.java
@@ -0,0 +1,7 @@
+public class project0 {
+    void main() {
+        // Print a custom message
+        //in the new java you dont need public static void, and you dont need the parameters string args! wowwwww
+        System.out.println("Hello, Class! This is Josh.");
+    }
+}
